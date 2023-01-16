b85f73588f5fd5a755d3137be43cc65460808cbc
unknown <brenthomas02@gmail.com>
2023-01-16 11:59:35 -0500

    Prompt users and Validate

--- Develope/script.js
+++ Develope/script.js
@@ -1,12 +1,45 @@
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+var lowercaseArr = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercaseArr = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numberArr = ["1234567890"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   console.log("Button 'Generate Password' was clicked");
+// 1. Prompt user for the password criteria.
+characterLength = parseInt(prompt("How many characters do you want your password to be? Between 8-128 characters"));
+
+  if(isNaN(characterLength) || characterLength < 8 || characterLength > 128){
+    alert("Character length input is invalid");
+    return false;
+  }
+
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowercaseArr);
+  }
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(uppercaseArr);
+  }
+
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  return console.log(choiceArr);
+// 3. Generate password based on criteria.
 
 
 
-
+// 4. Display the generated password on the page.
   return "Generated password will go here";
 }
 
